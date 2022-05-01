2f3d5db5625d731eb2e483525eb6ede2e9162da3
larsstrandos <lars@strandos-ikt.no>
2022-05-01 18:46:49 +0200

    Done some inital sriping and added i3-Gaps compiling

---
+++ root.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Change Debian to SID Branch
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp sources.list /etc/apt/sources.list
+
+
+# Add base packages
+apt install unzip picom sddm rofi kitty thunar flameshot neofetch feh git lxpolkit lxappearance xorg rofi -y
+apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
+
+# Build and install i3-gaps
+
+apt purge i3
+apt install meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+git clone https://github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+mkdir -p build && cd build
+meson --prefix /usr/local
+ninja
+sudo ninja install
+
+# Fira Code Nerd Font variant needed
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
+unzip Meslo.zip -d /usr/share/fonts
+fc-cache -vf
+
+cd $builddir
+mkdir -p /home/$username/.config
+cp .Xresources /home/$username
+cp .Xnord /home/$username
+cp -R dotfiles/* /home/$username/.config/
+chown -R $username:$username /home/$username
