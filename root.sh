c32a37cbf013b3f83bf15910b15741e90fe90d48
larsstrandos <lars@strandos-ikt.no>
2022-05-10 00:03:05 +0200

    Done some improvements

--- root.sh
+++ root.sh
@@ -29,62 +29,25 @@ mkdir /usr/share/xsessions/
 cp i3-gaps.desktop /usr/share/xsessions/i3-gaps.desktop
 
 # Add base packages
-apt install unzip picom sddm rofi kitty thunar flameshot polybar neofetch feh git lxpolkit lxappearance xorg rofi wget curl network-manager network-manager-fortisslvpn snapd -y
+apt install unzip picom sddm rofi kitty thunar flameshot polybar neofetch feh git lxpolkit lxappearance xorg rofi wget curl network-manager network-manager-fortisslvpn snapd debian-goodies xfce4-notifyd -y
 apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls nextcloud-desktop
- thunderbird steam remmina remmina-plugin-rdp remmina-plugin-secret vlc -y
+ thunderbird steam remmina remmina-plugin-rdp remmina-plugin-secret vlc code vim -y
 
-# Install spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-apt update
-apt install spotify-client -y
+# Install vivaldi web browser
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | dd of=/usr/share/keyrings/vivaldi-browser.gpg
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | dd of=/etc/apt/sources.list.d/vivaldi-archive.list
+apt update && apt install vivaldi-stable -y
 
-# Install Discord
-wget -O ./discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-apt install ./discord.deb -y
-
-# Install Teams
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
-apt update
-apt install teams -y
-
-# Install Onlyoffice
-snap install onlyoffice-desktopeditors -y
-
-# Install Vivaldi webbrowser
-apt update
-apt -y install wget gnupg2 software-properties-common
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
-add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' 
-apt update
-apt install vivaldi-stable -y
 
 # Install Wine direct from WineHQ
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor > /etc/apt/trusted.gpg.d/wine.gpg
+echo 'deb http://dl.winehq.org/wine-builds/debian/ bullseye main' > /etc/apt/sources.list.d/wine.list
 dpkg --add-architecture i386
-wget -qO- https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
-apt install software-properties-common && sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'
 apt update
-apt install --install-recommends winehq-devel -y
+apt install --install-recommends winehq-staging -y
 
 # Install Vulkan drivers
-apt install libvulkan1 libvulkan1:i386 -y
-
-# Install Lutris
-echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
-wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_11/Release.key -O- | sudo apt-key add -
-apt update
-apt install lutris -y
-
-# Install Heroic launcher
-bash <(wget -O- https://raw.githubusercontent.com/Heroic-Games-Launcher/HeroicGamesLauncher/main/madrepo.sh)
-apt install heroic -y
-
-# Install GNS3
-deb http://ppa.launchpad.net/gns3/ppa/ubuntu trusty main
-deb-src http://ppa.launchpad.net/gns3/ppa/ubuntu trusty main
-aptupdate
-apt install gns3-gui -y
+apt install libvulkan1 mesa-vulkan-drivers vulkan-utils libgl1-mesa-glx:i386 libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
 
 # Fira Code Nerd Font variant needed
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
@@ -92,6 +55,7 @@ unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 unzip Meslo.zip -d /usr/share/fonts
 fc-cache -vf
+apt install fonts-font-awesome -y
 
 # Allow to run reboot, poweroff and halt without sudo password and make alias
 echo '%sudo ALL = NOPASSWD: /sbin/halt, /sbin/reboot, /sbin/poweroff' >> /etc/sudoers
